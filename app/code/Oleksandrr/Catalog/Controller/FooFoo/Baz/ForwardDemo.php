cbc99784b07ce8621261ebf64a6940ce7feafafd
Oleksandr Radionov <oleksandrr@default-value.com>
2020-04-02 18:04:07 +0300

    few fix

--- app/code/Oleksandrr/Catalog/Controller/FooFoo/Baz/ForwardDemo.php
+++ app/code/Oleksandrr/Catalog/Controller/FooFoo/Baz/ForwardDemo.php
@@ -3,41 +3,25 @@ declare(strict_types=1);
 
 namespace Oleksandrr\Catalog\Controller\FooFoo\Baz;
 
-use Magento\Framework\App\Action\Context;
-use Magento\Framework\Controller\Result\ForwardFactory;
+use Magento\Framework\Controller\Result\Forward;
+use Magento\Framework\Controller\ResultFactory;
 
 class ForwardDemo extends \Magento\Framework\App\Action\Action implements \Magento\Framework\App\Action\HttpGetActionInterface
 {
-    /**
-     * @var \Magento\Framework\Controller\Result\ForwardFactory $forwardFactory
-     */
-    protected $forwardFactory;
-
-    public function __construct(
-        Context $context,
-        \Magento\Framework\Controller\Result\ForwardFactory $forwardFactory
-    ) {
-        parent::__construct($context);
-        $this->forwardFactory = $forwardFactory;
-    }
-
     /**
      * @inheritDoc
      * "https://alexandrr.local/oleksandrr-catalog-controllers-demo/fooFoo_baz/forwardDemo"
      */
     public function execute()
     {
-        $foo = false;
-        $response = $this->forwardFactory->create();
-        $response
-            ->setParams(
-                [
-                    'firstName' => 'Oleksandr',
-                    'lastName' => 'Radionov',
-                    'url' => 'Some Url',
-                ]
-            )
-            ->forward('dataDemo');
+        /** @var Forward $response */
+        $response = $this->resultFactory->create(ResultFactory::TYPE_FORWARD);
+        $response->setParams([
+            'first_name' => 'Oleksandr',
+            'last_name' => 'Radionov',
+            'url' => 'Some Url',
+        ])
+        ->forward('dataDemo');
 
         return $response;
     }
