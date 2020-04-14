59ed97539a45c748c849b3014f45159f953542d1
Oleksandr Radionov <oleksandrr@default-value.com>
2020-04-14 17:58:40 +0300

    add Observer user login

---
+++ app/code/Oleksandrr/Chat/Observer/UserLogin.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+namespace Oleksandrr\Chat\Observer;
+
+use Oleksandrr\Chat\Model\Message;
+
+class UserLogin implements \Magento\Framework\Event\ObserverInterface
+{
+    /**
+     * @var \Magento\Customer\Model\Session
+     */
+    private $userSession;
+
+    /**
+     * @var \Oleksandrr\Chat\Model\ResourceModel\Message\CollectionFactory
+     */
+    private $messageCollectionFactory;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var \Oleksandrr\Chat\Model\ResourceModel\Message
+     */
+    private $messageResource;
+
+    /**
+     * @param \Oleksandrr\Chat\Model\ResourceModel\Message\CollectionFactory $messageCollectionFactory
+     * @param \Oleksandrr\Chat\Model\ResourceModel\Message $messageResource
+     * @param \Magento\Customer\Model\Session $userSession
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(
+        \Oleksandrr\Chat\Model\ResourceModel\Message\CollectionFactory $messageCollectionFactory,
+        \Oleksandrr\Chat\Model\ResourceModel\Message $messageResource,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Customer\Model\Session $userSession
+    ) {
+        $this->messageCollectionFactory = $messageCollectionFactory;
+        $this->messageResource = $messageResource;
+        $this->logger = $logger;
+        $this->userSession = $userSession;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        try {
+            if ($chatHash = $this->userSession->getChatHash()) {
+                $user = $observer->getEvent()->getCustomer();
+                $messageCollection = $this->messageCollectionFactory->create();
+                $messageCollection->addChatHashFilter($chatHash)->addAuthorTypeFilter(1);
+
+                /** @var Message $message */
+                foreach ($messageCollection as $message) {
+                    if ((int) $message->getAuthorId() === 0) {
+                        $message->setAuthorId($user->getId());
+                        $this->messageResource->save($message);
+                    }
+                }
+            }
+        } catch (\Exception $e) {
+            $this->logger->critical($e);
+        }
+    }
+}
